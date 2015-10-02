32939fb0432e9f7a7a2d8aecb91b0bf2251d938d
Markus Graf <info@markusgraf.ch>
2015-10-02 21:34:31 +0200

    supporter class black box

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -1,3 +1,16 @@
 class Supporter
   include Mongoid::Document
+
+  field :first_name, type: String
+  field :last_name,  type: String
+  field :street,     type: String
+  field :zip,        type: Integer
+  field :email,      type: String
+  field :support,    type: String # Indicates the type of support
+  field :votable,    type: Boolean
+
+  field :age_category, type: String
+
+  validates :email,   presence: true, uniqueness: true
+  validates :support, presence: true
 end
