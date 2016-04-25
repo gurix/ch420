07bd06913e92e562b9fdd9e0833924a95fe96f3c
Markus Graf <info@markusgraf.ch>
2016-04-25 12:17:12 +0200

    makeup

--- lib/mail_chimp_batch_update.rb
+++ lib/mail_chimp_batch_update.rb
@@ -2,24 +2,23 @@ class MailChimpBatchUpdate
   def self.update_all(api_key, list_id)
     subscribers = []
     Supporter.each do |supporter|
-      subscriber = {
-        'EMAIL' => { 'email' => supporter.email },
-        'language' => supporter.language,
-        :EMAIL_TYPE => 'html',
-        :merge_vars => {
-          'EMAIL' => supporter.email,
-          'FNAME' => supporter.first_name,
-          'LNAME' => supporter.last_name,
-          'SUPPORT' => supporter.support,
-          'MEMBERSHIP' => supporter.li_membership.to_s,
-          'AGE' => supporter.age_category,
-          'LANG' => supporter.language
-        }
-      }
-      subscribers.push(subscriber)
+      subscribers.push(subscriber supporter)
     end
 
     mailchimp = Mailchimp::API.new(api_key)
     mailchimp.lists.batch_subscribe(list_id, subscribers, false, true, false)
+  end
+
+  def self.subscriber(supporter)
+    { 'EMAIL' => { 'email' => supporter.email }, 'language' => supporter.language, :EMAIL_TYPE => 'html',
+      :merge_vars => {
+        'EMAIL' => supporter.email,
+        'FNAME' => supporter.first_name,
+        'LNAME' => supporter.last_name,
+        'SUPPORT' => supporter.support,
+        'MEMBERSHIP' => supporter.li_membership.to_s,
+        'AGE' => supporter.age_category,
+        'LANG' => supporter.language
+      } }
   end
 end
