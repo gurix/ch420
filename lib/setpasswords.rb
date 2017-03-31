ecc5ac17b8335f2585e6a4ff0b074ce55a828fd3
Markus Graf <info@markusgraf.ch>
2017-03-31 18:26:30 +0200

    set passwort for nil sign ins

--- lib/setpasswords.rb
+++ lib/setpasswords.rb
@@ -1,4 +1,4 @@
 helper = ApplicationController.helpers
 
-never_signed_in = Supporter.where(:sign_in_count.lt => 1)
+never_signed_in = Supporter.where(sign_in_count: nil)
 never_signed_in.each_with_index {|u,i|puts "#{i} #{u.email}";  u.update_attributes password: "#{u.support}#{ helper.number_with_delimiter(u.zip, :delimiter => ',')}"}
