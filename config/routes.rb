5b069a1aa48e1b5bce6331357c4dc5faaea679d6
Markus Graf <info@markusgraf.ch>
2017-04-12 22:53:08 +0200

    Costs content

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     get '/arguments', to: 'pages#show', as: :arguments, page: 'arguments'
     get '/faq', to: 'pages#show', as: :faq, page: 'faq'
     get '/terms_and_conditions', to: 'pages#show', as: :terms_and_conditions, page: 'terms_and_conditions'
+    get '/sponsoring', to: 'pages#show', as: :sponsoring, page: 'sponsoring'
+    get '/numbers', to: 'pages#show', as: :numbers, page: 'numbers'
   end
 
   get '/cities_autocomplete', to: 'cities_autocomplete#index', as: :cities_autocomplete
