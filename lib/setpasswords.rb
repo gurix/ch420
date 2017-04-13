b30bd91f3d641b9a36ac50855a0ae5d062faa2db
Markus Graf <info@markusgraf.ch>
2017-04-13 10:05:09 +0200

    Makeup

--- lib/setpasswords.rb
+++ lib/setpasswords.rb
@@ -1,4 +1,7 @@
 helper = ApplicationController.helpers
 
 never_signed_in = Supporter.where(sign_in_count: nil)
-never_signed_in.each_with_index { |u, i| puts "#{i} #{u.email}"; u.update_attributes password: "#{u.support}#{helper.number_with_delimiter(u.zip, delimiter: ',')}" }
+never_signed_in.each_with_index do |u, i|
+  puts "#{i} #{u.email}"
+  u.update_attributes password: "#{u.support}#{helper.number_with_delimiter(u.zip, delimiter: ',')}"
+end
